355b7aeeaf49dac8cc9ede351d1439760a7ed165
Candrabirawa29 <damarproject10@gmail.com>
2026-06-17 22:15:27 +0700

    fix missing dome gallery section

--- app/page.js
+++ app/page.js
@@ -16,20 +16,17 @@ export default function Home() {
   const [isMusicStarted, setIsMusicStarted] = useState(false);
 
   return (
-    <main>
-      {/* 1. Gate Intro / Overlay Screen */}
-      {/* <CinematicOverlay onEnter={() => setIsMusicStarted(true)} /> */}
-
-      {/* 2. Audio Controller Instance */}
+    <main className="bg-black w-full min-h-screen">
+      {/* 1. Audio Controller Instance */}
       <MusicPlayer />
 
-      {/* 3. Main Cinematic Content */}
-      {/* Kita beri efek blur tipis/fade di konten utama jika user belum menekan enter (opsional, untuk menambah vibe) */}
-
+      {/* 2. Main Cinematic Content */}
       <HeroScene />
       <StoryIntro />
       <GalleryReel />
-      <div className="w-full min-h-screen relative block bg-black">
+      
+      {/* DomeGallery dikunci h-screen sendiri agar efek 3D-nya aman */}
+      <div className="w-full h-screen relative bg-black">
         <DomeGallery
           fit={0.8}
           minRadius={600}
@@ -38,11 +35,13 @@ export default function Home() {
           dragDampening={2}
           grayscale
         />
-        <MemoryWall />
-        <ApologySection />
-        <ThingsILovedSection />
-        <ClosingScene/>
       </div>
+
+      {/* Seksi sisanya ditaruh berurutan di bawahnya secara bebas */}
+      <MemoryWall />
+      <ApologySection />
+      <ThingsILovedSection />
+      <ClosingScene />
     </main>
   );
 }
